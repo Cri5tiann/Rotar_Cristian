7e3b3b75b217c786ffeb01630f2656289ae81397
Cristian <crotar2003@gmail.com>
2023-09-21 17:50:38 +0200

    Removed parameters from methods. used the variable radius inside my class as suggested

--- src/main/java/homework_nr_4/Circle.java
+++ src/main/java/homework_nr_4/Circle.java
@@ -1,16 +1,18 @@
 package homework_nr_4;
 
 public class Circle {
+
     public static void main(String[] args) {
-        Circle area = new Circle(5);
-        System.out.println(area);
+        Circle area = new Circle(8);
+    }
+    float radius;
+
+    public Circle(float radius) {
+        this.radius = radius;
     }
 
-    double radius;
-
-    public Circle(double radius){
-        this.radius = radius;
-        System.out.println("result is:" + 3.14 * (radius * radius));
+    public double calculateArea() {
+        return radius * radius * 3.1415;
     }
 
 }
