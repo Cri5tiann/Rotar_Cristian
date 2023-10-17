0163439602f39a3af56cd86a56cb32ae9c826b96
Cristian <crotar2003@gmail.com>
2023-10-18 00:28:52 +0200

    Used Streams API to filter and find elements from the list based on one or more characteristics.

---
+++ src/main/java/homework_nr_11/Person.java
@@ -0,0 +1,16 @@
+package homework_nr_11;
+
+public class Person {
+
+    String name;
+    String surname;
+    Integer age;
+    Boolean PaidByHour;
+
+    public Person(String name, String surname, Integer age, Boolean paidByHour) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        PaidByHour = paidByHour;
+    }
+}
